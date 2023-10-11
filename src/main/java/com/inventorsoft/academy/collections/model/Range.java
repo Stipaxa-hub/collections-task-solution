de7db95c5ae2a5496d3306980d0854ac12c4ed45
Stipaxa <petrivs572@gmail.com>
2023-10-11 22:22:16 +0300

    Fixed iterator. Optimized method size, isEmpty. Added throws exception in method which not supported. Added methods of for Numbers type data.

--- src/main/java/com/inventorsoft/academy/collections/model/Range.java
+++ src/main/java/com/inventorsoft/academy/collections/model/Range.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 
 import java.util.Set;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 public class Range<T extends Comparable<T>> implements Set<T> {
     private T start;
@@ -17,10 +18,22 @@ public class Range<T extends Comparable<T>> implements Set<T> {
         this.functionIncrementation = functionIncrementation;
     }
 
-    public static <R extends Comparable<R>> Range<R> of(R start, R finish) {
-        return new Range<>(start, finish, null);
+    public static Range<Integer> of(Integer start, Integer finish) {
+        return of(start, finish, (Integer number) -> number + 1);
     }
+
+    public static Range<Double> of(Double start, Double finish) {
+        return of(start, finish, (Double number) -> number + 0.1);
+    }
+
+    public static Range<Float> of(Float start, Float finish) {
+        return of(start, finish, (Float number) -> number + 0.1f);
+    }
+
     public static <R extends Comparable<R>> Range<R> of(R start, R finish, Function<R, R> function) {
+        if (start == null || finish == null || function == null) {
+            throw new RuntimeException("One of parameters is null! Parameters must be not null!");
+        }
         return new Range<>(start, finish, function);
     }
 
@@ -31,60 +44,41 @@ public class Range<T extends Comparable<T>> implements Set<T> {
         int size = 0;
         T current = start;
         while (current.compareTo(finish) <= 0) {
+            current = functionIncrementation.apply(current);
             size++;
-            if (functionIncrementation != null) {
-                current = functionIncrementation.apply(current);
-            } else {
-                current = increment(current);
-            }
         }
         return size;
     }
     public boolean isEmpty() {
-        return size() == 0;
+        return start.compareTo(finish) == 0;
     }
 
     public boolean contains(Object o) {
+        if (!(o instanceof Comparable)) {
+            return false;
+        }
         T value = (T) o;
         return value.compareTo(start) >= 0 && value.compareTo(finish) <= 0;
     }
 
     public Iterator<T> iterator() {
-        Iterator<T> iterator = new Iterator<T>() {
-            private T current = start;
-            @Override
-            public boolean hasNext() {
-                return current.compareTo(finish) <= 0;
-            }
-
-            @Override
-            public T next() {
-                T value = current;
-                if (functionIncrementation != null) {
-                    current = functionIncrementation.apply(current);
-                } else {
-                    current = increment(current);
-                }
-                return value;
-            }
-        };
-        return iterator;
+        return new RangeIterator();
     }
 
     public Object[] toArray() {
-        return new Object[0];
+        throw new UnsupportedOperationException("Operation creating array not supported");
     }
 
     public <T1> T1[] toArray(T1[] a) {
-        return null;
+        throw new UnsupportedOperationException("Operation creating array not supported");
     }
 
     public boolean add(T t) {
-        return false;
+        throw new UnsupportedOperationException("Operation adding not supported");
     }
 
     public boolean remove(Object o) {
-        return false;
+        throw new UnsupportedOperationException("Operation removing not supported");
     }
 
     public boolean containsAll(Collection<?> c) {
@@ -93,29 +87,33 @@ public class Range<T extends Comparable<T>> implements Set<T> {
     }
 
     public boolean addAll(Collection<? extends T> c) {
-        return false;
+        throw new UnsupportedOperationException("Operation adding not supported");
     }
 
     public boolean retainAll(Collection<?> c) {
-        return false;
+        throw new UnsupportedOperationException("Operation retaining not supported");
     }
 
     public boolean removeAll(Collection<?> c) {
-        return false;
+        throw new UnsupportedOperationException("Operation removing not supported");
     }
 
     public void clear() {
+        throw new UnsupportedOperationException("Operation clearing not supported");
     }
 
-    private T increment(T current) {
-        if (current instanceof Integer) {
-            return (T) (Integer.valueOf(((Integer) (current)) + 1));
-        } else if (current instanceof Float) {
-            return (T) (Float.valueOf(((Float) current) + 0.1f));
-        } else if (current instanceof Double) {
-            return (T) (Double.valueOf(((Double) current) + 0.1));
-        } else {
-            throw new RuntimeException("Illegal type!");
+    private class RangeIterator implements Iterator<T> {
+        private T current = start;
+        @Override
+        public boolean hasNext() {
+            return current.compareTo(finish) <= 0;
+        }
+
+        @Override
+        public T next() {
+            T value = current;
+            current = functionIncrementation.apply(current);
+            return value;
         }
     }
 }
